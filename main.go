cfcbc90e48d827e6fb6327101d07ee54ef254f7b
fanialfi <fanialfirdaus@gmail.com>
2025-01-06 11:30:42 +0700

    web server routing static assets

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	message := "welcome"
-	w.Write([]byte(message))
-}
-
-func handleHello(w http.ResponseWriter, r *http.Request) {
-	message := "hello world"
-	w.Write([]byte(message))
-}
-
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/index", handleIndex)
-	http.HandleFunc("/hello", handleHello)
+	http.Handle("/static/",
+		http.StripPrefix("/static",
+			http.FileServer(http.Dir("assets"))))
 
-	address := "localhost:3000"
-	log.Printf("server started at %s\n", address)
-	server := new(http.Server)
-	server.Addr = address
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("error :", err.Error())
-	}
+	fmt.Println("server started at localhost:3000")
+	http.ListenAndServe(":3000", nil)
 }
