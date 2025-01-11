529beaa4c17fd2d3bcb961b3198b00bbad668e07
fanialfi <fanialfirdaus@gmail.com>
2025-01-11 22:27:09 +0700

    create & using middleware using DefaultServeMux

--- main.go
+++ main.go
@@ -1,29 +1,38 @@
 package main
 
+// middleware menggunakan DefaultServeMux
+
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/student", ActionStudent)
+	// http.ServeMux adalah sebuah router yang semua router dibuat akan didaftarkan di sini
+	mux := http.DefaultServeMux
 
+	mux.HandleFunc("/student", ActionStudent)
+
+	// registrasi middleware
+	// karena http.ServeMux juga mengimplementasikan interface http.Handler
+	// http.serveMux digunakan untuk menghandle semua permintaan dari clien dan diteruskan ke router terkait
+	var handler http.Handler = mux
+	handler = MiddlewareAuth(handler)
+	handler = MiddlewareAllowOnlyGET(handler)
+
+	// http.Server adalah server http-nya itu sendiri yang bertanggung jawab
+	// untuk mendengarkan request dari port tertentu dan kemudian request itu akan diteruskan ke Handler
+	// dalam hal ini request akan diteruskan ke ServeMux
 	server := new(http.Server)
 	server.Addr = ":9000"
-	fmt.Println("server started at localhost:9000")
+	server.Handler = handler
+
+	log.Println("server started at localhost:9000")
 	server.ListenAndServe()
 }
 
 func ActionStudent(w http.ResponseWriter, r *http.Request) {
-	if !Auth(w, r) {
-		return
-	}
-
-	if !AllowOnlyGET(w, r) {
-		return
-	}
-
 	if id := r.URL.Query().Get("id"); id != "" {
 		OutputJSON(w, SelectStudent(id))
 		return
--- middleware.go
+++ middleware.go
@@ -7,27 +7,29 @@ const (
 	PASSWORD = "1234"
 )
 
-func Auth(w http.ResponseWriter, r *http.Request) bool {
-	username, password, ok := r.BasicAuth()
-	if !ok {
-		w.Write([]byte("terjadi kesalahan"))
-		return false
-	}
+func MiddlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			w.Write([]byte("something went wrong"))
+			return
+		}
 
-	isValid := (username == USERNAME) && (password == PASSWORD)
-	if !isValid {
-		w.Write([]byte("wrong username or password"))
-		return false
-	}
-
-	return true
+		isValid := (username == USERNAME) && (password == PASSWORD)
+		if !isValid {
+			w.Write([]byte("username or password is wrong"))
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
-func AllowOnlyGET(w http.ResponseWriter, r *http.Request) bool {
-	if r.Method != http.MethodGet {
-		w.Write([]byte("hanya mengizinkan GET"))
-		return false
-	}
-
-	return true
+func MiddlewareAllowOnlyGET(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.Write([]byte("only support GET method"))
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
